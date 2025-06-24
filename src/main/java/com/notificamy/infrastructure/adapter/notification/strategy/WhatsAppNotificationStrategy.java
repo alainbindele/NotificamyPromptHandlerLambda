983bedb8e0b872ab40be74a70321c5cd0d994579
Alain <alain.bindele@gmail.com>
2025-06-24 16:30:44 +0200

    fixed key names

--- src/main/java/com/notificamy/infrastructure/adapter/notification/strategy/WhatsAppNotificationStrategy.java
+++ src/main/java/com/notificamy/infrastructure/adapter/notification/strategy/WhatsAppNotificationStrategy.java
@@ -30,7 +30,7 @@ public class WhatsAppNotificationStrategy implements NotificationStrategy {
     @ConfigProperty(name = "app.whatsapp.api-url")
     String whatsappApiUrl;
     
-    @ConfigProperty(name = "app.aws.secrets.whatsapp.api.key")
+    @ConfigProperty(name = "app.aws.secrets.api-keys")
     String apiKeysSecretName;
     
     private final ObjectMapper objectMapper;
